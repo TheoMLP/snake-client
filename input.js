3b7154deb8db4b79665edfff7faa18f13946ecf4
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-28 20:27:59 +0000

    WASD Movement done

--- input.js
+++ input.js
@@ -1,12 +1,21 @@
-const handleUserInput = (key) => {
+const handleUserInput = (key, connection) => {
   if (key === "\u0003") {
     process.exit();
+  } else if (key === "\u007A") {
+    connection.write("Move: up");
+  } else if (key === "\u0073") {
+    connection.write("Move: down");
+  } else if (key === "\u0071") {
+    connection.write("Move: left");
+  } else if (key === "\u0064") {
+    connection.write("Move: right");
   }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  let connection = conn
   const stdin = process.stdin
-  stdin.on("data", key => handleUserInput(key))
+  stdin.on("data", key => handleUserInput(key, connection))
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
