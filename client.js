803de39d47327f909a8995f0126b26617017a23c
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-28 19:22:03 +0000

    move commands

--- client.js
+++ client.js
@@ -1,17 +1,32 @@
 const net = require("net");
 
+
 const connect = function(){
   const conn = net.createConnection({
-    host: "localhost",
+    host: "192.168.0.102",
     port: 50541
   });
   
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
+  const nameAndMove = () => {
     console.log("Successfully connected to the game server");
     conn.write("Name: THM")
-  })
+    conn.write("Move: up, Move: up, Move: up, Move: up")
+  }
+
+  // const moveSnake = () => {
+  //   const moveList = ["Move: up", "Move: up", "Move: up", "Move: up", "Move: up", "Move: right", "Move: right", "Move: right", "Move: right", "Move: right", "Move: down", "Move: down", "Move: down", "Move: down", "Move down", "Move: left", "Move: left", "Move: left", "Move: left", "Move left"]
+  //   let time = 200
+  //   for (let move of moveList) {
+  //     setTimeout(() => {
+  //       conn.write(move)}, time)
+  //       time += 75
+  //   }
+  // } 
+  
+  conn.setEncoding("utf8");
+
+  conn.on("connect", moveSnake)
+  
   conn.on("data", (data) => {
     console.log(data);
   })
