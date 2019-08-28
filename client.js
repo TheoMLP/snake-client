0ea8774b64fc41016478298d4a54a18c30edb114
TheoMLP <theomalaper.cognez@gmail.com>
2019-08-28 18:40:53 +0000

    added snake initials

--- client.js
+++ client.js
@@ -8,8 +8,12 @@ const connect = function(){
   
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server");
+    conn.write("Name: THM")
+  })
   conn.on("data", (data) => {
-    console.log(data)
+    console.log(data);
   })
   return conn;
 };
